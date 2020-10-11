6d5353ce17af59aeffadb619bcdf828220e671ea
Nicolas van Kempen <nvankemp@gmail.com>
2020-10-11 10:11:35 -0400

    Initial commit.

---
+++ server/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Using POST can be tricky with vanilla Node.js, so here is a snippet to get you started.
+// function (req, res) {
+//     // ...
+
+//     let body = '';
+//     req.on('data', data => body += data);
+//     req.on('end', () => {
+//         // The request has been fully processed, and you can use / parse the body variable.
+//         // ...
+//     });
+//
+//     // ...
+// }
+
+
+// TODO
